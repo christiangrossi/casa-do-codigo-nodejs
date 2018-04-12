a8ec91088ff1daf1534b484c1e6e550618990987
unknown <christian.grossi@outlook.com>
2018-04-12 17:29:27 -0300

    listando produtos após salvamento - send.redirect()

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,14 +1,26 @@
 module.exports = function(app){
-    app.get('/produtos', function(req, res){
-        
+    var listaProdutos = function(req, res){
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.produtosBanco(connection);
-       
-        produtosBanco.lista(function(err, results){
+        var ProdutosDAO = new app.infra.ProdutosDAO(connection);
+        ProdutosDAO.lista(function(err, results){
             res.render('produtos/lista',{ lista:results })
         });
-
         connection.end();
+    }
 
+    app.get('/produtos', listaProdutos);
+
+    app.get('/produtos/form', function(req, res){
+        res.render('produtos/form');
     });
+
+    app.post('/produtos/salva',function(req, res){
+        var produto = req.body;
+        var connection = app.infra.connectionFactory();
+        var ProdutosDAO = new app.infra.ProdutosDAO(connection);
+        ProdutosDAO.salvar(produto, function(err, results){
+            res.redirect('/produtos');
+        });
+    });
+
 }
--- config/express.js
+++ config/express.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
+
 var app = express();
 
 // como esse código não vai se carregado o tempo inteiro, podemos deixa-lo
@@ -8,6 +10,8 @@ module.exports = function (){
     // set -> definir varíaveis pro express
     app.set('view engine','ejs');// seta ejs como engine
     app.set('views','./app/views');
+    app.use(bodyParser.urlencoded({extended: true}));
+
     load('routes',{cwd: 'app'})
         .then('infra')
     .into(app);

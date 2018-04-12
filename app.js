d4d51cadda967364988378f6d94ca7f590320c99
unknown <christian.grossi@outlook.com>
2018-04-12 11:39:29 -0300

    NodeJS - EJS e Nodemon

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 
 var app = require('express')();
+
+// set -> definir varíaveis pro express
+// setando ejs como engine
+app.set('view engine','ejs');
+
+// rotas
+app.get('/', function(req, res){
+    res.send("Voce esta na home");
+})
+
+app.get('/produtos', function(req, res){
+    console.log('listando');
+    res.render("produtos/lista");
+})
+
 app.listen(3000, function(){
     console.log('Servidor ouvindo na porta 3000');
 })

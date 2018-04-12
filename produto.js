449b0ce95510ae43241c2fe8aec27ad7b776f361
unknown <christian.grossi@outlook.com>
2018-04-12 10:54:52 -0300

    criação de server NodeJs

--- produto.js
+++ produto.js
@@ -2,6 +2,8 @@ var http = require('http');
 
 
 let server = http.createServer(function(req,res){
-    res.end("<h1></h1>");
+    res.end("<h1>Hello World</h1>");
 });
 server.listen(3000);
+
+console.log("Servidor rodando");

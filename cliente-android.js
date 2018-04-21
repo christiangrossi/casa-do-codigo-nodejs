8ce32ce40bb8900367583d6feb3c5e418dca7114
unknown <christian.grossi@outlook.com>
2018-04-21 19:55:21 -0300

    NodeJS - Content Negotiation

---
+++ cliente-android.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+var configuracoes= {
+    hostname:'localhost',
+    port: 3000,
+    path: '/produtos'
+};
+
+http.get(configuracoes,function(res){
+    console.log(res.statusCode);
+    res.on('data',function(body){
+        console.log('Corpo: ' + body);
+    });
+    res.on('error', function(erro){
+        console.log(error);
+    })
+});
